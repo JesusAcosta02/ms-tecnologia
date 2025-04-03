f1fcc7698a9befadb7273c3d2eda92de2bdfa547
JesusPersonal <jeacoh02@gmail.com>
2025-04-02 22:55:44 -0500

    ✅ feat: implement HU2 - list technologies with pagination and sorting
    Added ListTechnologiesUseCase interface and implementation.
    Integrated pagination and sorting logic using DatabaseClient.
    Added new endpoint GET /technologies in TechnologyController.
    Included default query parameters: page, size, sortBy, ascending.
    Implemented TechnologyEntityMapper.fromRow() for manual mapping.
    Updated tests in TechnologyControllerTest.

--- src/main/java/com/onclass/mstecnologia/application/usecase/impl/ListTechnologiesUseCaseImpl.java
+++ src/main/java/com/onclass/mstecnologia/application/usecase/impl/ListTechnologiesUseCaseImpl.java
@@ -1,15 +1,10 @@
 package com.onclass.mstecnologia.application.usecase.impl;
 
-import com.onclass.mstecnologia.application.api.TechnologyDTO;
 import com.onclass.mstecnologia.application.usecase.ListTechnologiesUseCase;
 import com.onclass.mstecnologia.domain.model.Technology;
 import com.onclass.mstecnologia.domain.repository.TechnologyRepository;
-import com.onclass.mstecnologia.infrastructure.adapters.db.mappers.TechnologyMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 
 @Service
